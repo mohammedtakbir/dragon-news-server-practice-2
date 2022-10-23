4f6b3f59a78823fd390b7501d6f33e4e05890eff
mohammedtakbir <takbirhossain678@gmail.com>
2022-10-23 22:22:28 +0600

    news categories, selected news API created

--- index.js
+++ index.js
@@ -5,10 +5,30 @@ const port = process.env.PORT || 5001;
 const cors = require('cors');
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('testing the server')
+const categories = require('./data/category.json');
+const news = require('./data/news.json');
+
+app.get('/', (req, res) =>{
+    res.send('testing the server for the second time');
 })
 
+app.get('/news-categories', (req, res) =>{
+    res.send(categories)
+})
+
+app.get('/categories/:id', (req, res) =>{
+    const id = req.params.id;
+    const newsCategories = news.filter(n => n.category_id === id);
+    res.send(newsCategories)
+})
+
+app.get('/news/:id', (req, res) =>{
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews)
+})
+
+
 app.listen(port, () =>{
-    console.log('running the server on port', port);
+    console.log('running the server')
 })
